9a1bb29fb9b9d03c2af4a43d14b9b91272b9d6cc
AndresQuimbita <paulquimbita@hotmail.es>
2023-10-30 20:51:44 -0500

    Try CART and ID3

--- classifier.py
+++ classifier.py
@@ -12,10 +12,14 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.naive_bayes import GaussianNB
-from chefboost import Chefboost as chef
+# from chefboost import Chefboost as chef
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
+from sklearn.metrics import confusion_matrix, classification_report
+
 
 class Classifier:
-    
+
     def __init__(self, path, csv):
 
         self.df = pd.read_csv(csv)
@@ -23,14 +27,13 @@ class Classifier:
 
         # Input_ y_Target_Variable.
         self.y = self.df.iloc[:, -1]
-        
+
         # Normalize
         scaler = MinMaxScaler()
         # scaler.fit(self.x)
         self.x_scaled = scaler.fit_transform(self.x)
 
-
-    def classify(self, x_scaled, x, y, df, config):
+    def classify(self, x_scaled, x, y, df, criterion):
         # Create StratifiedKFold object.
         skf = StratifiedKFold(n_splits=10, shuffle=True, random_state=42)
         lst_accu_stratified = []
@@ -39,57 +42,70 @@ class Classifier:
         recall_list = []
         auc_list = []
 
+        model = DecisionTreeClassifier(
+            criterion=criterion, max_depth=None, random_state=42)
+
         for train_index, test_index in skf.split(x_scaled, y):
             x_train_fold, x_test_fold = x_scaled[train_index], x_scaled[test_index]
             y_train_fold, y_test_fold = y[train_index], y[test_index]
-            model = chef.fit(self.df, config = config, target_label = 'label')
-            
+            # model = chef.fit(self.df, config = config, target_label = 'label')
+
+            tree.plot_tree(model.fit(x_train_fold, y_train_fold))
+
             # classifier.fit(x_train_fold, y_train_fold)
             # lst_accu_stratified.append(classifier.score(x_test_fold, y_test_fold))
 
-            #Revisar 
-            #Hacer greatSearch
+            # Revisar
+            # Hacer greatSearch
             y_pred = model.predict(x_test_fold)
-            
+
             # Metrics
-            # mse = mean_squared_error(y_test_fold, y_pred)
-            # accu_list.append(accuracy_score(y_test_fold, y_pred))
-            # precision_list.append(precision_score(y_test_fold, y_pred))
-            # recall_list.append(recall_score(y_test_fold, y_pred))
-            # auc_list.append(roc_auc_score(y_test_fold, y_pred))
+            accu_list.append(accuracy_score(y_test_fold, y_pred))
+            precision_list.append(precision_score(y_test_fold, y_pred))
+            recall_list.append(recall_score(y_test_fold, y_pred))
+            auc_list.append(roc_auc_score(y_test_fold, y_pred))
 
         # Print the output.
         # print('List of possible accuracy:', lst_accu_stratified)
         # print('\nMaximum Accuracy That can be obtained from this model is:',
-        #     max(lst_accu_stratified)*100, '%')
+        #       max(lst_accu_stratified)*100, '%')
         # print('\nMinimum Accuracy:',
-        #     min(lst_accu_stratified)*100, '%')
+        #       min(lst_accu_stratified)*100, '%')
         # print('\nOverall Accuracy:',
-        #     mean(lst_accu_stratified)*100, '%')
+        #       mean(lst_accu_stratified)*100, '%')
         # print('\nStandard Deviation is:', stdev(lst_accu_stratified))
 
-        # print('Metrics Results:')
-        # print('-'*30)
-        # print(f"Accuracy: {mean(accu_list)*100:.2f}% ± {stdev(accu_list):.2f}")
-        # print(f"Precision: {mean(precision_list)*100:.2f}% ± {stdev(precision_list):.2f}")
-        # print(f"Recall: {mean(recall_list)*100:.2f}% ± {stdev(recall_list):.2f}")
-        # print(f"AUC: {mean(auc_list)*100:.2f}% ± {stdev(auc_list):.2f}\n")
+        print('Metrics Results:')
+        print('-'*30)
+        print(f"Accuracy: {mean(accu_list)*100:.2f}% ± {stdev(accu_list):.2f}")
+        print(
+            f"Precision: {mean(precision_list)*100:.2f}% ± {stdev(precision_list):.2f}")
+        print(
+            f"Recall: {mean(recall_list)*100:.2f}% ± {stdev(recall_list):.2f}")
+        print(f"AUC: {mean(auc_list)*100:.2f}% ± {stdev(auc_list):.2f}\n")
 
-        # return np.mean(accu_list)
+        cm = confusion_matrix(y_test_fold, y_pred)
+
+        print('Confusion Matrix:')
+        print(cm)
+
+        print(classification_report(y_test_fold, y_pred))
+
+        return np.mean(accu_list)
 
     def run(self):
 
         print('##############################################################       ID3')
 
-        configID3 = {'algorithm': 'ID3'}
-        self.classify(self.x_scaled, self.x, self.y, self.df, configID3)
-        
-        print('##############################################################       C4.5')
-        
-        configC45 = {'algorithm': 'C4.5'}
-        self.classify(self.x_scaled, self.x, self.y, self.df, configC45)
-        
+        criterion = 'entropy'
+        self.classify(self.x_scaled, self.x, self.y, self.df, criterion)
+
+        # print('##############################################################       C4.5')
+
+        # configC45 = {'algorithm': 'C4.5'}
+        # self.classify(self.x_scaled, self.x, self.y, self.df, configC45)
+
         print('##############################################################       CART')
-       
-        configCART = {'algorithm': 'CART'}
-        self.classify(self.x_scaled, self.x, self.y, self.df, configCART)
+
+        criterion = 'gini'
+        self.classify(self.x_scaled, self.x, self.y, self.df, criterion)
